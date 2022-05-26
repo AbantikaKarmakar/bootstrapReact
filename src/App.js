6743ad014e1d178ffef25d07c3e8011358bd5df3
Abantika Karmakar <85815714+AbantikaKarmakar@users.noreply.github.com>
2022-05-26 11:56:49 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Accordion from './component/accordian/Accordion';
+
+const App = () => {
+    return (
+        <>
+            <Accordion />
+        </>
+    );
+};
+
+export default App;
